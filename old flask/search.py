aa9ef0ddce29dc31c452b88722a478d2a4eb928e
Joshua Wootonn <joshua-wootonn@uiowa.edu>
2017-09-17 13:26:05 -0500

    Deployed to gh-pages

--- old flask/search.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class Search(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
